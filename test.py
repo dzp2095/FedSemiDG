0666f15edb262d7818468b7ba656de483fac2730
Zhipeng <zdengaj@connect.ust.hk>
2024-10-17 17:47:09 +0800

    fix

--- test.py
+++ test.py
@@ -4,16 +4,20 @@ from glob import glob
 from pathlib import Path
 import os
 
-pred_img_path = "/storage/zhipengdeng/data/segmentation/cardiac/fed_semi/client_1/data/A0S9V9/img_vis/result/fl_cardiac_fixmatch_labeled_[2, 3, 4]_unseen_1_round_95_client_1/3_0_dice_0.9328.png"
-original_img_path = "/storage/zhipengdeng/data/segmentation/cardiac/fed_semi/client_1/data/A0S9V9/img_vis/3_0.png"
-
 pred_img_path = "/storage/zhipengdeng/data/segmentation/cardiac/fed_semi/*/data/*/img_vis/result/*/*.png"
 pred_img_files = glob(pred_img_path)
 
 for pred_img_file in pred_img_files:
     file_name = os.path.basename(pred_img_file)
+    if 'dice' not in file_name:
+        continue
     original_file_name = file_name.split('_dice')[0] + '.png'
     original_img_path = Path(pred_img_file).parents[2].joinpath(original_file_name)
+    save_file_name = Path(pred_img_file).parent.joinpath(f'{file_name.split("_dice")[0]}_new.png')
+    # if save_file_name exists, skip
+    if os.path.exists(save_file_name):
+        print(f"Skip {save_file_name}")
+        continue
 
     pred_vis = Image.open(pred_img_file)
     original_img_path = Image.open(original_img_path)
@@ -44,7 +48,6 @@ for pred_img_file in pred_img_files:
 
     new_pred_img = ((new_pred_img + original_img_np)* (0.5,0.5,0.5)).astype(np.uint8)
 
-    save_file_name = Path(pred_img_file).parent.joinpath(f'{file_name.split("_dice")[0]}_new.png')
     Image.fromarray(new_pred_img).save(save_file_name)
     print(f"Saved {save_file_name}")
 
