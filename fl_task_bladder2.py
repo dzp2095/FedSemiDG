fdc064edb2cd6810130dde604ce95e18ce5fbdf7
Zhipeng <zdengaj@connect.ust.hk>
2024-12-14 23:41:27 +0800

    update

--- fl_task_bladder2.py
+++ fl_task_bladder2.py
@@ -7,23 +7,25 @@ all_unseen_sites = [4, 3, 2, 1]
 task = "bladder"
 data_dir = "bladder"
 
-runtime = 3
-for run in range(runtime):
-    for i in range(len(all_labeled_sites)):
-        labeled_sites = all_labeled_sites[i]
-        unseen_site = all_unseen_sites[i]
-        run_name = f"fl_{task}_fl_label_only_labeled_{labeled_sites}_unseen_{unseen_site}_run_{run}"
-        labeled_clients = ["client_" + str(client_id) for client_id in labeled_sites]
-        unseen_client = f'client_{unseen_site}'
-        command = [
-            "python3", "fl_train.py",
-            "--config", f"/zhipengdeng/project/fed_semi/configs/{task}/fl_run_conf.yaml",
-            "--run_name", run_name,
-            "--train_path", f"/storage/zhipengdeng/data/segmentation/{data_dir}/fed_semi",
-            "--test_path", f"/storage/zhipengdeng/data/segmentation/{data_dir}/fed_semi",
-            "--trainer", "supervised",
-            "--unseen_client", unseen_client,
-            "--labeled_clients"
-        ] + labeled_clients
+for i in range(len(all_labeled_sites)):
+    labeled_sites = all_labeled_sites[i]
+    unseen_site = all_unseen_sites[i]
+    flw = 0.0
+    fpr = 0.0
+    run_name = f"fl_{task}_ours_no_dg_labeled_{labeled_sites}_unseen_{unseen_site}"
+    labeled_clients = ["client_" + str(client_id) for client_id in labeled_sites]
+    unseen_client = f'client_{unseen_site}'
+    command = [
+        "python3", "fl_train.py",
+        "--config", f"/zhipengdeng/project/fed_semi/configs/{task}/fl_run_conf.yaml",
+        "--run_name", run_name,
+        "--train_path", f"/storage/zhipengdeng/data/segmentation/{data_dir}/fed_semi",
+        "--test_path", f"/storage/zhipengdeng/data/segmentation/{data_dir}/fed_semi",
+        "--trainer", "semi",
+        "--use_ga",
+        "--unseen_client", unseen_client,
+        "--labeled_clients"
+    ] + labeled_clients + ["--feature_loss_weight", str(flw), "--fp_rate", str(fpr)]
 
-        subprocess.run(command)
+    subprocess.run(command)
+
--- fl_task_bladder4.py
+++ fl_task_bladder4.py
@@ -6,10 +6,10 @@ all_unseen_sites = [4, 3, 2, 1]
 
 task = "bladder"
 data_dir = "bladder"
-feature_loss_weight = [0.5]
-fp_rate = [0.5]
-entropy_start_ratio = [0.2, 0.3]
-entropy_end_ratio = [0.4]
+feature_loss_weight = [0.1, 0.3, 0.5]
+fp_rate = [0.3, 0.5]
+entropy_start_ratio = [0.1, 0.2, 0.3]
+entropy_end_ratio = [0.6]
 
 for flw, fpr, esr, eer in itertools.product(feature_loss_weight, fp_rate, entropy_start_ratio, entropy_end_ratio):
     for i in range(len(all_labeled_sites)):
